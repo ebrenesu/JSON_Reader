51640a4bdf244e8dfe3aae07dc0667f97c9e8beb
Erick B (cenfo) <43499120+ebrenesu@users.noreply.github.com>
2020-06-11 10:03:43 -0600

    json proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as bisoft12 from '../assets/classData.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'JSonReader';
+export class AppComponent implements OnInit {
+  title = 'JSon Reader';
+
+  objJsClase: any = (bisoft12 as any).default;
+  temasInvestigacion: any = this.objJsClase["Temas investigación"];
+  estudiantes: any = this.objJsClase["Estudiantes"];
+
+  constructor(){}
+  ngOnInit(){
+    console.log(bisoft12);
+  }
 }
